fb4497ffcdb64b1d5c5863821b4ae66b91489073
root-wings <118381754+root-wings@users.noreply.github.com>
2023-04-17 17:35:05 +0800

    Create openssl_build.sh

---
+++ openssl_build.sh
@@ -0,0 +1,86 @@
+#!/bin/bash -e
+
+WORK_PATH=$(cd "$(dirname "$0")";pwd)
+ANDROID_NDK_PATH=${WORK_PATH}/android-ndk-r20b
+OPENSSL_SOURCES_PATH=${WORK_PATH}/openssl-3.1.0
+ANDROID_TARGET_API=$1
+ANDROID_TARGET_ABI=$2
+OUTPUT_PATH=${WORK_PATH}/openssl_3.1.0_${ANDROID_TARGET_ABI}
+
+OPENSSL_TMP_FOLDER=/tmp/openssl_${ANDROID_TARGET_ABI}
+mkdir -p ${OPENSSL_TMP_FOLDER}
+cp -r ${OPENSSL_SOURCES_PATH}/* ${OPENSSL_TMP_FOLDER}
+
+function build_library {
+    mkdir -p ${OUTPUT_PATH}
+    make && make install
+    rm -rf ${OPENSSL_TMP_FOLDER}
+    rm -rf ${OUTPUT_PATH}/bin
+    rm -rf ${OUTPUT_PATH}/share
+    rm -rf ${OUTPUT_PATH}/ssl
+    rm -rf ${OUTPUT_PATH}/lib/engines*
+    rm -rf ${OUTPUT_PATH}/lib/pkgconfig
+    rm -rf ${OUTPUT_PATH}/lib/ossl-modules
+    echo "Build completed! Check output libraries in ${OUTPUT_PATH}"
+}
+
+if [ "$ANDROID_TARGET_ABI" == "armeabi" ]
+then
+    export ANDROID_NDK_ROOT=${ANDROID_NDK_PATH}
+    PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:$PATH
+    cd ${OPENSSL_TMP_FOLDER}
+    ./Configure android-arm -D__ANDROID_API__=${ANDROID_TARGET_API} -static no-shared no-tests --prefix=${OUTPUT_PATH}
+    build_library
+
+elif [ "$ANDROID_TARGET_ABI" == "armeabi-v7a" ]
+then
+    export ANDROID_NDK_ROOT=${ANDROID_NDK_PATH}
+    PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:$PATH
+    cd ${OPENSSL_TMP_FOLDER}
+    ./Configure android-arm -D__ANDROID_API__=${ANDROID_TARGET_API} -static no-shared no-tests --prefix=${OUTPUT_PATH}
+    build_library
+
+elif [ "$ANDROID_TARGET_ABI" == "arm64-v8a" ]
+then
+    export ANDROID_NDK_ROOT=${ANDROID_NDK_PATH}
+    PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:$PATH
+    cd ${OPENSSL_TMP_FOLDER}
+    ./Configure android-arm64 -D__ANDROID_API__=${ANDROID_TARGET_API} -static no-shared no-tests --prefix=${OUTPUT_PATH}
+    build_library
+
+elif [ "$ANDROID_TARGET_ABI" == "mips" ]
+then
+    export ANDROID_NDK_ROOT=${ANDROID_NDK_PATH}
+    PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:$PATH
+    cd ${OPENSSL_TMP_FOLDER}
+    ./Configure android-mips -D__ANDROID_API__=${ANDROID_TARGET_API} -static no-shared no-tests --prefix=${OUTPUT_PATH}
+    build_library
+
+elif [ "$ANDROID_TARGET_ABI" == "mips64" ]
+then
+    export ANDROID_NDK_ROOT=${ANDROID_NDK_PATH}
+    PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:$PATH
+    cd ${OPENSSL_TMP_FOLDER}
+    ./Configure android-mips64 -D__ANDROID_API__=${ANDROID_TARGET_API} -static no-shared no-tests --prefix=${OUTPUT_PATH}
+    build_library
+
+elif [ "$ANDROID_TARGET_ABI" == "x86" ]
+then
+    export ANDROID_NDK_ROOT=${ANDROID_NDK_PATH}
+    PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:$PATH
+    cd ${OPENSSL_TMP_FOLDER}
+    ./Configure android-x86 -D__ANDROID_API__=${ANDROID_TARGET_API} -static no-shared no-tests --prefix=${OUTPUT_PATH}
+    build_library
+
+elif [ "$ANDROID_TARGET_ABI" == "x86_64" ]
+then
+    export ANDROID_NDK_ROOT=${ANDROID_NDK_PATH}
+    PATH=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin:$ANDROID_NDK_ROOT/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:$PATH
+    cd ${OPENSSL_TMP_FOLDER}
+    ./Configure android-x86_64 -D__ANDROID_API__=${ANDROID_TARGET_API} -static no-shared no-tests --prefix=${OUTPUT_PATH}
+    build_library
+
+else
+    echo "Unsupported target ABI: $ANDROID_TARGET_ABI"
+    exit 1
+fi
